bc546596379d0ff93c2c1336a36655bf4afa910e
Kevin Watters <kevinwatters@gmail.com>
2018-06-28 00:28:37 -0400

    variable assignment and references

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,24 @@ extern crate cretonne_codegen;
 extern crate cretonne_frontend;
 #[macro_use] extern crate target_lexicon;
 
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::Read;
+use std::str::FromStr;
+use failure::Error;
+
 use cretonne_codegen::verifier::verify_function;
 use cretonne_faerie::{FaerieBackend, FaerieBuilder, FaerieTrapCollection};
 use cretonne_module::{DataContext, Linkage, Module, Writability};
 use cretonne::prelude::{FunctionBuilder, Variable, InstBuilder, Value,
     AbiParam, settings, FunctionBuilderContext,
-    Configurable, codegen, isa};
-use std::str::FromStr;
-use failure::Error;
+    Configurable, codegen, isa, EntityRef};
 
 #[derive(Debug)]
 pub enum Expr {
     IntegerLiteral(String),
+    Ref(String),
+    Assign(String, Box<Expr>),
     Add(Box<Expr>, Box<Expr>),
     Sub(Box<Expr>, Box<Expr>),
     Mul(Box<Expr>, Box<Expr>),
@@ -48,11 +52,40 @@ fn make_builder(name: &str) -> FaerieBuilder {
 struct Translator<'a> {
     builder: FunctionBuilder<'a, Variable>,
     module: &'a mut Module<FaerieBackend>,
+    names: &'a mut HashMap<String, Variable>,
 }
 
 impl<'a> Translator<'a> {
+    fn declare_variables(&mut self, exprs: &[Expr]) {
+        let mut variable_names = HashSet::new();
+        for expr in exprs {
+            match *expr {
+                Expr::Assign(ref name, _) => {
+                    variable_names.insert(name);
+                }
+                _ => {}
+            }
+        }
+
+        for (index, variable_name) in variable_names.iter().enumerate() {
+            let x = Variable::new(index);
+            self.builder.declare_var(x, self.module.pointer_type());
+            self.names.insert(variable_name.to_string(), x);
+        }
+    }
+
     fn translate_expr(&mut self, expr: Expr) -> Value {
         match expr {
+            Expr::Assign(name, expr) => {
+                let new_value = self.translate_expr(*expr);
+                let variable = self.names.get(&name).expect(&format!("no var named '{}'", name));
+                self.builder.def_var(*variable, new_value);
+                new_value
+            }
+            Expr::Ref(name) => {
+                let variable = self.names.get(&name).unwrap();
+                self.builder.use_var(*variable)
+            }
             Expr::Add(lhs, rhs) => {
                 let lhs = self.translate_expr(*lhs);
                 let rhs = self.translate_expr(*rhs);
@@ -85,7 +118,6 @@ fn main() -> Result<(), Error> {
     let mut input = String::new();
     File::open("hello.eck")?.read_to_string(&mut input)?;
 
-
     let mut module = Module::<FaerieBackend>::new(make_builder("test.o"));
     let int = module.pointer_type();
 
@@ -113,30 +145,27 @@ fn main() -> Result<(), Error> {
 
     {
         let builder = FunctionBuilder::<Variable>::new(&mut context.func, &mut func_builder_ctx);
-        let mut trans = Translator { module: &mut module, builder: builder };
+        let mut names = HashMap::new();
+        let mut trans = Translator { module: &mut module, builder: builder, names: &mut names };
         let entry_ebb = trans.builder.create_ebb();
         trans.builder.append_ebb_params_for_function_params(entry_ebb);
         trans.builder.switch_to_block(entry_ebb);
+        trans.builder.seal_block(entry_ebb);
+
+        let items = parser::module(&input).unwrap_or_else(|e| {
+            eprintln!("error parsing: {:?}", e);
+            std::process::exit(1);
+        });
+        trans.declare_variables(&items);
 
         let return_value = {
-            match parser::module(&input) {
-                Ok(items) => {
-                    println!("parsed: {:?}", items);
-                    let mut last_value:Value = Value::with_number(0).unwrap(); // TODO: how to not have a value
-                    for item in items {
-                        last_value = trans.translate_expr(item);
-                    }
-                    last_value
-                },
-                Err(e) => {
-                    eprintln!("error parsing: {:?}", e);
-                    std::process::exit(1);
-
-                }
+            println!("parsed: {:?}", items);
+            let mut last_value:Value = Value::with_number(0).unwrap(); // TODO: how to not have a value
+            for item in items {
+                last_value = trans.translate_expr(item);
             }
+            last_value
         };
-
-        trans.builder.seal_block(entry_ebb);
 
         trans.builder.ins().return_(&[return_value]);
         trans.builder.finalize();
